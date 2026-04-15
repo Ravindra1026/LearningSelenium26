e065b5a3a76193e4897bf800e44de682b09c8fc5
Ravindra Singh <rathoreravindra1026@gmail.com>
2026-04-15 08:28:49 +0530

    15 April 2026

---
+++ src/test/java/org/example/ex09_15042026/WebTables.java
@@ -0,0 +1,51 @@
+package org.example.ex09_15042026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
+public class WebTables {
+    @Test
+    public void testMethod(){
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--start-maximized");
+        edgeOptions.addArguments(("--inprivate"));
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://awesomeqa.com/webtable.html");
+
+        //  Table = //table[@id = 'customers']
+        // row = //table[@id = 'customers']/tbody/tr
+        // column = //table[@id = 'customers']/tbody/tr[2]/td
+        // //tbody/tr[]/td[]
+
+        int row = driver.findElements(By.xpath("//table[@id = 'customers']/tbody/tr")).size();
+        int col = driver.findElements(By.xpath("//table[@id = 'customers']/tbody/tr[2]/td")).size();
+
+        System.out.println(row);
+        System.out.println(col);
+
+        String first_part = "//table[@id = 'customers']/tbody/tr[";
+        String second_part = "]/td[";
+        String third_part = "]";
+
+        for (int i = 2; i < row; i++){
+            for (int j = 1; j < col; j++){
+                String dynamic_xpath = first_part + i + second_part + j + third_part;
+                String data = driver.findElement(By.xpath(dynamic_xpath)).getText();
+                if (data.contains("Helen Bennett")){
+                    String country_path = dynamic_xpath + "/following-sibling::td";
+                    String country_text = driver.findElement(By.xpath(country_path)).getText();
+                    System.out.println("____________");
+                    System.out.println("Helen Bennett is in - " + country_text);
+                }
+
+            }
+        }
+
+
+        driver.quit();
+    }
+}
